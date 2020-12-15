604949c4f8b027cd330d0785898d7c4c5da65907
alamsamudrow <76034571+alamsamudrow@users.noreply.github.com>
2020-12-15 18:43:46 +0700

    Add files via upload

---
+++ data/titikacuan_1.js
@@ -0,0 +1,8 @@
+var json_titikacuan_1 = {
+"type": "FeatureCollection",
+"name": "titikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.410035275632453, -6.983551788563874 ] } }
+]
+}
